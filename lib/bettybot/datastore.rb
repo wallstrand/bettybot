c982a5eaa002ceae25a142e24a2ac51ea90753c9
Sven Dahlstrand <sven.dahlstrand@gmail.com>
2011-10-08 21:27:36 +0200

    Add support for heroku.

--- lib/bettybot/datastore.rb
+++ lib/bettybot/datastore.rb
@@ -5,7 +5,15 @@ module Bettybot
   class Datastore
 
     def initialize(collection)
-      @db = Mongo::Connection.new.db 'bettybot'
+      if ENV['MONGOHQ_URL']
+        uri = URI.parse(ENV['MONGOHQ_URL'])
+        conn = Mongo::Connection.new(uri.host, uri.port)
+        @db = conn.db(uri.path.gsub(/^\//, ''))
+        @db.authenticate(uri.user, uri.password)
+      else
+        @db = Mongo::Connection.new.db 'bettybot'
+      end
+
       @collection = @db.collection(collection)
     end
 
